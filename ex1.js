25f99c84a45b10cba85d4b8f136a2c0f81cf6ecb
Matt Grieco <griecomc@dukes.jmu.edu>
2013-02-13 22:08:48 -0500

    Added form validations and stylings

---
+++ ex1.js
@@ -0,0 +1,46 @@
+//function that validates street
+function validateStreet()
+{
+//gets data from input form
+var x=inputform.stad1.value;
+//if there is no data, then say that it must be filled out
+if (x==null || x=="")
+  {
+  alert("Street Address must be filled out!");
+  return false;
+  }
+}
+function validateCity()
+{
+var x=inputform.city.value;
+if (x==null || x=="")
+  {
+  alert("City must be filled out!");
+  return false;
+  }
+}
+function validateEmail()
+{
+var x=inputform.useremail.value;
+//next few lines specify that there must be @ and . characters in the input
+//variable for @ sign, at the first occurrence of @
+var atpos=x.indexOf("@");
+//variable for the "." in .com
+var dotpos=x.lastIndexOf(".");
+//the first character can't be @, the . position from the end has to be 2 from it
+if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+  {
+  alert("Not a valid e-mail address");
+  return false;
+  }
+}
+
+function validateZip()
+{
+if (isNaN(inputform.state.value))
+	{
+	alert ("Please enter numbers into zipcode box")
+	return false;
+	}
+}
+//w3schools was used for help with the code!
